d9e0b98c4eddeb57bc9ab0d270d3b38163880047
Adrian Popa <popadrian94@gmail.com>
2019-03-11 19:56:42 +0200

    first commit

---
+++ Intro.js
@@ -0,0 +1,66 @@
+console.info("My First JS File")
+
+var employed = true;
+var age = 24;
+var name = 'Adrian'
+
+console.warn("employed");
+console.warn(employed);
+
+console.info("name:");
+console.info(name);
+
+name = "Popa";
+
+console.info("name:");
+console.info(name);
+
+function getWelcomeMsg(name) {
+    var msg = "Hello " + name + "," + " welcome to FastTrackIT.";
+    return msg;
+}
+var info = getWelcomeMsg("Adrian");
+console.info(info)
+
+function getHello() {
+    return "Welcome to my website!";
+}
+function getColor() {
+    var date = new Date();
+    console.log(date.getMinutes());
+    if (date.getMinutes() > 18) {
+        return "green"
+    }
+    return "blue";
+}
+var Welcome = getHello();
+console.info(Welcome)
+
+var summaryElement = document.getElementById("summary");
+console.info("summaryElement", summaryElement)
+var color = getColor();
+summaryElement.style.color = color;
+
+// first example
+function hideHomePage() {
+    document.getElementById('home-page').style.display = 'none'
+}
+function showSkillsPage() {
+    document.getElementById('skills-page').style.display = 'block';
+}
+function clickOnSkills() {
+    //hideHomePage();
+    //showSkillsPage();
+    hidePage('home-page');
+    hidePage('languages-page');
+    hidePage('about-page');
+    showPage('skills-page');
+}
+// first example - end
+
+function hidePage(page) {
+    document.getElementById(page).style.display = 'none';
+}
+function showPage(page) {
+    document.getElementById(page).style.display = 'block';
+}
